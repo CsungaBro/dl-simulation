b95520857600a859ac298ae6cd8905f30a0969de
CsungaBro <csunga16@gmail.com>
2022-03-21 11:13:10 +0100

    70% workin + cleaner code

--- ls-dyna-automatization/geom_gen_func.py
+++ ls-dyna-automatization/geom_gen_func.py
@@ -1,11 +1,8 @@
 import regex as re
-import os 
 
 class Geometric_Parameters():
     def __init__(self):
-        self.main_parameters_range = {
-            "number_of_sim" : 1
-        }
+        self.main_parameters_range = {}
         self.main_parameters = {
         # base parameters
         "z0" : 10,
@@ -50,31 +47,55 @@ class Geometric_Parameters():
         pass
 
     def main(self):
-        for num in range(self.main_parameters_range["number_of_sim"]):
-            self.calculate_main_parameters()
-            self.calculate_derived_parameters()
-            self.all_parameters.update(self.main_parameters)
-            self.all_parameters.update(self.derived_parameters)
-            self.all_parameters_container.append(self.all_parameters)
-            self.all_parameters = {}
+        self.all_parameters.update(self.main_parameters)
+        self.all_parameters.update(self.derived_parameters)
+        self.all_parameters_container.append(self.all_parameters)
 
 class Main():
-    def __init__(self,generator_file, output_path, all_parameters_cointainer):
+    def __init__(self,generator_file, output_file, all_parameters_cointainer):
         self.generator_file = generator_file
-        self.output_path = output_path
+        self.output_file = output_file
         self.output_names = []
         self.geom_part = True
-        self.all_parameters_cointainer = all_parameters_cointainer
+        self.all_params_array = all_parameters_cointainer
         self.parameters = {}
-        self.output_names_generator()
+        # self.parameters = {
+        # # base parameters
+        # "z0" : 10,
+        # # Stamp/Stempel parameters
+        # "h1" : 15 ,
+        # "z1" : 33,
+        # "a1" : 48,
+        # "b1" : 48,
+        # "r1" : 10,
+        # "mesh_max_1" : 3,
+        # "mesh_min_1" : 1,
+        # "mesh_dev_1" : 0.5,
+        # # Matrize parameters
+        # "h2" : 20,
+        # "z2" : 30 ,
+        # "a2" : 120,
+        # "b2" : 120,
+        # "c2" : 50,
+        # "d2" : 50,
+        # "r2_u" : 5,
+        # "r2_b" : 10,
+        # "mesh_max_2" : 6,
+        # "mesh_min_2" : 2,
+        # "mesh_dev_2" : 1,
+        # # Organoblech parameters
+        # "a3" : 80,
+        # "b3" : 80,
+        # "z3" : 31,
+        # "mesh_max_3" : 3,
+        # "mesh_min_3" : 1,
+        # "mesh_dev_3" : 0.5}        
 
-    def output_names_generator(self):
-        for count, parameters in enumerate(self.all_parameters_cointainer):
-            self.output_names.append("sim_{}_{}x{}_R{}.cfile".format(count, parameters["a2"], parameters["b2"], parameters["r2_b"]))
+    def dict_getter(self):
+        self.parameters = self.all_params_array[0]
 
-
-    def generate_c_file(self, output_file):
-        with open(self.generator_file, "r") as f_r, open(output_file,"w") as f_w:
+    def generate_c_file(self):
+        with open(self.generator_file, "r") as f_r, open(self.output_file,"w") as f_w:
             for line in f_r:
                 f_w.write(self.process(line))
         self.geom_part = True
@@ -90,23 +111,13 @@ class Main():
             if parameter in new_line:
                 new_line = re.sub(parameter,str(self.parameters[parameter]),new_line)
         return new_line
-
-    def main(self):
-        for output_name, parameters in zip(self.output_names, self.all_parameters_cointainer):
-            self.parameters = parameters
-            self.generate_c_file(os.path.join(self.output_path, output_name))
-
-
     
 if __name__ == "__main__":
     # input_file = "ls-dyna-automatization\\template\\k_file_gen.cfile"
-    # input_file = "ls-dyna-automatization\\template\\save_2.cfile"
-    # output_file = "ls-dyna-automatization\\output"
-    input_file = "template\\save_2.cfile"
-    output_path = "output"
-    
+    input_file = "ls-dyna-automatization\\template\\save_2.cfile"
+    output_file = "ls-dyna-automatization\\output\\test2.cfile"
 
-    P = Geometric_Parameters()
+    P = Geometric_Parameters
 
-    M = Main(input_file, output_path, P.all_parameters_container)
-    M.main()
+    M = Main(input_file, output_file, P.all_parameters_container)
+    M.generate_c_file()
