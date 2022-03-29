87a4fe1287337a869bd423716aa12ece9262e904
CsungaBro <csunga16@gmail.com>
2022-03-29 17:31:56 +0200

    hmmm

--- ls-dyna-automatization/ls_c_runner.py
+++ ls-dyna-automatization/ls_c_runner.py
@@ -18,6 +18,10 @@ class FileHandler:
         c_file_path = os.path.join(self.c_file_folder_path, self.c_files[self.c_files_processed])
         k_file_name =re.sub("cfile$", "k", self.c_files[self.c_files_processed])
         k_file_path = os.path.join(self.k_file_folder_path, k_file_name)
+        try:
+            k_file_folder =os.makedirs()
+        except:
+            print("OH NO")
         self.c_files_processed += 1
         print(c_file_path, k_file_path)
         return c_file_path, k_file_path
@@ -31,12 +35,45 @@ class PowershellRunner:
         print(c_file_path, c_file_path)        
         print(k_file_path, k_file_path)  
 
+
+
     def fafo(self, c_file_path: str):
         print("FAFO")
         print(c_file_path)
-        completed = subprocess.run(["powershell", "-Command", c_file_path], capture_output=True)
-        
+        env = {"PATH": self.lspp_powershell_env_path}
+        command_1 = 'cd "D:/Program Files/ANSYS 2020R2 LS-DYNA Student 12.0.0/LS-DYNA"\n'
+        command_2 = "Set-Alias lspp $ENV:ANSYS_STUDENT_LSDYNA_LSPREPOST_PATH\n"
+        command_3 = 'cd "C:/Users/CsungaBro/Google Drive/TU Braunschweig/3. Semester/COURSE 0 - Studienarbeit/33-dl-sim/dl-simulation/ls-dyna-automatization"\n'
+        command_4 = "lspp c={} -nographics".format(c_file_path)
+        commands = [command_1, command_2, command_3, command_4]
+        psc_2 = "test_2.ps1"
+        with open(psc_2, "w") as f:
+            for command in commands:
+                f.write("{}".format(command))
+        psc_1 = "test.ps1"
+        # print(commands)
+        completed1 = subprocess.run(["powershell", ".\{}".format(psc_2)], capture_output=True)
+        # completed1 = subprocess.run(["powershell", command_1], capture_output=True)
+        # completed2 = subprocess.run(["powershell", command_2], capture_output=True)
+        # completed3 = subprocess.run(["powershell", command_3], capture_output=True)
+        # completed4 = subprocess.run(["powershell", command_4], capture_output=True)
 
+        # completed = subprocess.run(["powershell", "-Command", command_1, command_2, command_3, command_4], capture_output=True)
+        print(completed1)
+    def fafo2(self, k_file_path: str):
+        command_1 = 'cd "D:/Program Files/ANSYS 2020R2 LS-DYNA Student 12.0.0/LS-DYNA"\n'
+        command_2 = "Set-Alias lsrun $ENV:ANSYS_STUDENT_LSDYNA_LSRUN_PATH\n"
+        command_3 = 'cd "C:/Users/CsungaBro/Google Drive/TU Braunschweig/3. Semester/COURSE 0 - Studienarbeit/33-dl-sim/dl-simulation/ls-dyna-automatization/output/k_files/01"\n'
+        command_4 = "lsrun i={}".format(k_file_path)        
+        # command_4 = "lsrun i={} ncpu=4 memory=20m".format(k_file_path)        
+        commands = [command_1, command_2, command_3, command_4]
+        psc_3 = "test_3.ps1"
+        with open(psc_3, "w") as f:
+            for command in commands:
+                f.write("{}".format(command))
+        psc_1 = "test.ps1"
+        # print(commands)
+        completed1 = subprocess.run(["powershell", ".\{}".format(psc_3)], capture_output=True)
 
 class ScriptRunner:
     def __init__(self, number_of_files, FileHandler, PowershellRunner ):
@@ -63,3 +100,4 @@ if __name__ == "__main__":
     # SC.k_file_generator()
     
     PR.fafo("output\\c_files\\test.cfile")
+    PR.fafo2("C:\\Users\\CsungaBro\\Google Drive\\TU Braunschweig\\3. Semester\\COURSE 0 - Studienarbeit\\33-dl-sim\\dl-simulation\\ls-dyna-automatization\\output\\k_files\\01\\plsss.k")
---
+++ ls-dyna-automatization/test_3.ps1
@@ -0,0 +1,4 @@
+cd "D:/Program Files/ANSYS 2020R2 LS-DYNA Student 12.0.0/LS-DYNA"
+Set-Alias lsrun $ENV:ANSYS_STUDENT_LSDYNA_LSRUN_PATH
+cd "C:/Users/CsungaBro/Google Drive/TU Braunschweig/3. Semester/COURSE 0 - Studienarbeit/33-dl-sim/dl-simulation/ls-dyna-automatization/output/k_files/01"
+lsrun i=C:\Users\CsungaBro\Google Drive\TU Braunschweig\3. Semester\COURSE 0 - Studienarbeit\33-dl-sim\dl-simulation\ls-dyna-automatization\output\k_files\01\plsss.k
