154265a99ba1db9a1328ae42821105b8cd5335ff
CsungaBro <csunga16@gmail.com>
2022-07-20 22:07:10 +0200

    camera changed for curve generation

--- ls-dyna-automatization/post_proc/post_proc_curv_gen.py
+++ ls-dyna-automatization/post_proc/post_proc_curv_gen.py
@@ -30,9 +30,29 @@ class CurvatureGenerator(ABC):
 class PyVistaCurvatureGenerator(CurvatureGenerator):
     def __init__(self, method='mean') -> None:
         super().__init__(method)
-        self.camera_pos = [
-            (39.0, 39.0, 247.63193047090098), 
-            (39.0, 39.0, 32.09499931335449), 
+        self.a_1 = 80
+        self.b_1 = 80
+
+    def size_getter(self, base_name):
+        nums = re.findall("[0-9]+[.][0-9]+", base_name)
+        return float(nums[0]), float(nums[1])
+
+    def camera_pos_calc(self, a_2, b_2):
+        #camera pos_bas
+        x_1 = self.a_1/2-1
+        y_1 = self.b_1/2-1
+        #camera pos new
+        x_2 = a_2/2-1
+        y_2 = b_2/2-1
+        #delta between the positions
+        dx_3 = abs(x_2-x_1)
+        dy_3 = abs(y_2-y_1)  
+        # new cam pos
+        x_3 = x_2-dx_3*1
+        y_3 = y_2-dy_3*1
+        return [
+            (x_3, y_3, 260), 
+            (x_3, y_3, 41), 
             (0.0, 1.0, 0.0)]
 
     def curvature_generation(self, output_path) -> None:
@@ -43,7 +63,10 @@ class PyVistaCurvatureGenerator(CurvatureGenerator):
         curv = mesh.curvature(curv_type="mean")
         mesh_actor = self.add_actor_to_plotter(mesh, curv)
         self.plotter.view_xy()
-        self.plotter.camera_position = self.camera_pos        
+        file_name =os.path.basename(self.stl_path)
+        a_2, b_2 = self.size_getter(file_name)
+        self.plotter.camera_position = self.camera_pos_calc(a_2, b_2)        
+        self.plotter.remove_scalar_bar()
         self.plotter.show(screenshot=output_path)
         self.remove_actor_to_plotter(mesh_actor)
 
