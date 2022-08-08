47587fa74b618f7dd1021939c24ee5675238bd85
CsungaBro <csunga16@gmail.com>
2022-08-08 22:13:45 +0200

    1,2,3,4,6,8 generated

--- ls-dyna-automatization/powershell_scripts/lspp_powershell.ps1
+++ ls-dyna-automatization/powershell_scripts/lspp_powershell.ps1
@@ -1,2 +1,2 @@
 Set-Alias lspp $ENV:ANSYS_STUDENT_LSDYNA_LSPREPOST_PATH
-lspp c=output\k_files\sim_1087_25.45x63.64x15.27_R8.45\sim_1087_25.45x63.64x15.27_R8.45_stl_gen.cfile -nographics
+lspp c=output\k_files\sim_99_20.0x25.45x20.0_R5.18\sim_99_20.0x25.45x20.0_R5.18_stl_gen.cfile -nographics
